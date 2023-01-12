cbfc44773d70d265574b0f60efa685173d49aff2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 11:42:02 -0300

    Create ex06.py

---
+++ exercicios/ex06.py
@@ -0,0 +1,7 @@
+"""
+Escreva um programa que contém o numero PI, que deve
+ter o valor exato.(3.14159265359).Agora formate esse 
+numero para ter apenas cinco casas decimais.
+"""
+pi = 3.14159265359
+print('o numero PI formatado é %.5f' %(pi))
