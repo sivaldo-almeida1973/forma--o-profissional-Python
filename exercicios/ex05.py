62126d4e03ae1eb866cda490bde5eb682de59f69
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 11:41:59 -0300

    Create ex05.py

---
+++ exercicios/ex05.py
@@ -0,0 +1,10 @@
+"""
+escreva e formate a hora e minuto atual.
+não esqueça de criar 2 variaveis .
+
+"""
+hora = 11
+minuto = 33
+
+relogio = 'A hora atual é %d:%d minutos' %(hora, minuto)
+print(relogio)
