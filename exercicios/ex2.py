3ae27208159de9430caf3a7e81e51b440767f735
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 10:09:18 -0300

    alterei exercicio1.py

---
+++ exercicios/ex2.py
@@ -0,0 +1,17 @@
+"""
+crie variaveis para guardar seu nome, cpf e uma que indi-
+que se voce esta casado , em seguida printe esses valores
+separadamente, mas não esqueça de printar junto o que  
+significam.
+"""
+
+var_nome = input('digite seu nome: ')
+var_cpf = int(input('digite seu cpf: '))
+var_est_civil = input('digite seu estado civil: ')
+
+
+print(f' {var_nome}') 
+print( f'{var_cpf}')
+print(f'{var_est_civil} ')
+
+print(f'seu nome é {var_nome} seu cpf é {var_cpf} e seu estdo civil é {var_est_civil}' )
---
+++ exercicios/exercicio1.py
@@ -0,0 +1,16 @@
+"""
+Crie uma variável de cada tipo e ponha algun valor 
+escolhido.Em seguida, printe todos esses valores
+"""
+
+var_int = 10
+var_decim = 1.90
+var_text = "Olá, mundo"
+var_bool = True
+var_non = None
+
+print(var_int)
+print(var_decim)
+print(var_text)
+print(var_bool)
+print(var_non)
