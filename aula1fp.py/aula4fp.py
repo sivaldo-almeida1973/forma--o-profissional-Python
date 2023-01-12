8d5fc71df57d594757323b81a7d482b811053a23
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 10:35:07 -0300

    Create aula4fp.py

---
+++ aula1fp.py/aula4fp.py
@@ -0,0 +1,22 @@
+"""
+Formatação
+# %s texto
+# %d inteiro
+# %f real
+"""
+#nome = 'Alice'
+#texto_form = 'O nome dela é %s ' % (nome)
+#print(texto_form)
+
+nome = 'Alice'
+idade = 22
+altura = 1.70
+texto = 'meu nome é %s tenho %d e tenho %.2f  metros de latura ' % (nome,idade, altura, )
+print(texto)
+
+valor = False
+print('o valor é %s' %(valor))
+print('o valor é %d' %(valor))
+
+decimal = 23.44556
+print('a parte inteira é %d' %(decimal))
