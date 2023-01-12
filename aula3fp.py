abade9b9f9564e41f870559704d65554127d0fb6
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 09:38:56 -0300

    Create aula3fp.py

---
+++ aula3fp.py
@@ -0,0 +1,33 @@
+"""
+variaveis do tipo primitivo
+
+"""
+variavel = None
+print(variavel)
+
+inteiro = 10
+print(inteiro)
+
+decimal = 1.53
+print(decimal)
+
+texto = "Olá , mundo"
+print(texto)
+
+var = True
+print(var)
+
+var = False
+print(var)
+
+var1 = 10
+var2 = var1
+print(var2)
+
+var = None
+
+#padrão de variaveis
+
+saldoBancario = 100 
+SaldoBancario = 100 
+saldo_bancario =100
