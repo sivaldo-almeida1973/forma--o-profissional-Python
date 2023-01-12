03cdb9d1b7a2ad11a53cf53fd2b1ee385b087721
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-11 21:25:54 -0300

    Create fundamentos.py

---
+++ aula1fp.py/fundamentos.py
@@ -0,0 +1,19 @@
+#comentário
+#isto é um comentário
+
+"""
+esta é uma forma de comentar mais de uma linha
+"""
+
+print('Olá, mundo')#isto é um comentário
+print(10)
+print('20.5')
+print('maçã', 29, 10.5)
+
+print('Maçã','Pera','Uva',sep=' - ')#separa por traços (sep="-")
+print('Maçã','Pera','Uva',end= ' Fim ')#fim no final (end="fim")
+print('esta frase tem uma quebra,\n de linha')#quebra de liha (\n)
+print('Maçã','Pera','Uva',sep='\n') #iprime um em cada linha
+print('A pontuação total de %s foi %s pontos' % ('lucas', 10))
+print('A pontuaçao total de {} foi {} pontos '.format('lucas', 10))
+print('A pontuação total de ' +'lucas ' ' foi ' + ' 10 ' + ' pontos ')
