373eac4c0fc8b3780c284c9dafd62197ff2a5523
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 15:27:14 -0300

    Create aula07.py

---
+++ aula07.py
@@ -0,0 +1,20 @@
+"""
+                Operadores Lógicos 
+---------------------------------------------------
+aplicados a operações lógicas e tem como resultado .
+verdadeiro(True) ou Falso(False)
+AND: verdadeiro se todos os valores forem verdades 
+ se não será falso
+-True AND True => True
+- True AND False => False
+- False AND False => False
+---------------------------------------------------
+OR:verdadeiro se qualquer um for verdadeiro
+-True OR false => true
+-True OR True => True
+-False Or False => False
+----------------------------------------------------
+NOT: inverte o resultado lógico
+Not(True) => False
+Not(False)=> True
+"""
