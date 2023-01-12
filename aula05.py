4d8691c9aa546f2f359dec107bf188aa0cc7281f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 11:58:59 -0300

    Create aula05.py

---
+++ aula05.py
@@ -0,0 +1,37 @@
+"""
+OPreradores Aritiméticos
+
+"""
+print(10 + 20)#adição
+print('-'*10)
+
+numero = 10-20#subtrção
+print(numero)
+
+print('-'*10)
+outro_numero = 30 + numero
+print(outro_numero)
+
+print('-'*10)
+numero = 20-10
+print(numero)
+
+print('-'*10)
+numero = 10 * 2#multiplicação
+print(numero)
+
+print('-'*10)
+numero = 100 / 2#divisão
+print(numero)
+
+print('-'*10)
+numero = 10 // 3 #divisão de inteiros
+print(numero)
+
+print('-'*10)
+numero = 9 % 2 #resto da divisão
+print(numero)
+
+print('-'*10)
+numero = 2 ** 4 #exponenciação
+print(numero)
