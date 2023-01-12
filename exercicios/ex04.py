a6a147bc092b43282555ef1fa2a93f00bda2fa93
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 11:41:46 -0300

    Create ex04.py

---
+++ exercicios/ex04.py
@@ -0,0 +1,14 @@
+"""
+Escreva e formate a data em que você nasceu no formato 
+dia/mes/ano. Não esqueça de criar 3 variaveis para guardar
+cada um.
+
+"""
+
+dia = 10 
+mes = 11
+ano = 1973
+data = 'Eu nasci em %d/%d/%d'%(dia,mes,ano)
+print(data)
+
+
