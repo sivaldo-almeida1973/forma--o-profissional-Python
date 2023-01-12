2f69df5368ca6a669a68c168194841f9d274e773
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 13:14:47 -0300

    Create ex10.py

---
+++ exercicios/ex10.py
@@ -0,0 +1,10 @@
+ovos = 95
+pessoas =12
+print('tenos inicialmente %d avos para dividir pos %d pessoas ' % (ovos, pessoas))
+
+ovos_pessoas = ovos / pessoas
+print('cada uma das %d pessoas ficara com %d ovos ' % (pessoas,ovos_pessoas))
+
+resto = 95 % 10
+print("O restante é %d " %(resto))
+
