a561db3556ef3dd1af7b2fd3c77626d6a07e5b89
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-12 19:11:14 -0300

    Update ex08.py

--- exercicios/ex08.py
+++ exercicios/ex08.py
@@ -3,3 +3,34 @@ num2 = 20
 num3 = 30
 media = (10  + 20 + 30) / 3
 print('A media é %.0f' %(media))
+
+print('-'*20)
+
+numero = 123
+texto = str(numero)
+num_digitos = len(texto)
+print('O numero  %s tem %d digitos ' %(texto, num_digitos))
+
+print('-'*20)
+
+vazio = None
+numero1 = 15
+numero2 = 0
+texto = 'texto'
+texto_vazio = ''
+decimal_zero = 0.0
+decimal = 3.5
+
+print('A variavel tem valor:', bool(vazio))
+
+print('numero tem valor:', bool(numero1))
+print('numero tem valor:', bool(numero2))
+
+print('string tem conteudo:', bool(texto))
+print('string tem conteudo:', bool(texto_vazio))
+
+print('float tem valor:', bool(decimal_zero))
+print('float tem valor:', bool(decimal))
+
+
+
