ff89963c3e7ded164671820875679789f4451643
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-13 09:30:06 -0300

    Create aula09.py

---
+++ aula09.py
@@ -0,0 +1,14 @@
+#valor_escrito = input('digite seu nome:')
+#print(type(valor_escrito))
+#print('eu me chamo %s' %(valor_escrito))#
+
+#dia = input('Digite o dia: ')
+#mes = input('digite o mes: ')
+#ano = input('digite o ano: ')
+#print("a data exata é %s/%s/%s"  % (dia, mes, ano))
+
+entrada_usuario = input('Digite 1 para verdadeiro e 0 para falso: ')
+valor_inteiro = int(entrada_usuario)
+valor_logico = bool(valor_inteiro)
+print('voce escolheu: %s' % valor_logico)
+print('ou ainda, voce escolheu: %i' % valor_inteiro)
