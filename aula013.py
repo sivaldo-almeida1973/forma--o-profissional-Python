50bbff654676f78157f2d1ec0c5cc78e5ee2ee54
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-13 19:11:53 -0300

    Create aula013.py

---
+++ aula013.py
@@ -0,0 +1,32 @@
+#estudando If e Else:
+
+numero = 11
+if (numero >= 10):
+    print(' É  maior ou igual a 10')
+    print('>>>>>>')
+else:
+    print('É menor que 10')
+    print('--------')
+
+print('fim')
+
+
+print('-'*20)
+
+numero = 10
+if numero % 2 == 0:
+    print(' É Par')
+else:
+    print('É impar')
+
+valor = 1
+if valor >= 0 and valor < 10:
+    print(valor)
+else:
+    print('valor não aceito!')
+
+texto = 'aa'
+if len(texto) == 1:
+    print('texto tem 1 caractere')
+else:
+    print('tem mais de um caractere')
