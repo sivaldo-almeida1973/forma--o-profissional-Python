4dbe5381ffe533c74e6bbdbb124cad81fa036dd2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-13 10:15:57 -0300

    Create ex013.py

---
+++ exercicios/ex013.py
@@ -0,0 +1,4 @@
+num1 = input('digite um numero: ')
+num2 = input('digite outro numero: ')
+divisao = int(num1) / int(num2)
+print('o resultado da divisão de %s por %s  é %.2f'% (num1, num2, divisao))
