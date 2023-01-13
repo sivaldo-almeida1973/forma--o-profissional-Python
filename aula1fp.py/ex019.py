f9e30bb80069d9202442db864cb620854a52d8dc
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-13 17:01:59 -0300

    Delete ex019.py

--- aula1fp.py/ex019.py
+++
@@ -1,5 +0,0 @@
-#se texto possui vogal
-
-texto = input('digite um palavra:')
-possui_vogal = ('a'in texto) or ('e' in texto)or ('i' in texto) or ('o' in texto) or('u' in texto)
-print('possui vogal?',possui_vogal)
