c0a245aa9af782efb10347cf2b9aa6421cd58020
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-13 10:16:01 -0300

    Create ex014.py

---
+++ exercicios/ex014.py
@@ -0,0 +1,8 @@
+dia = input('Dia que voce nasceu: ')
+mes = input('Mes que voce nasceu: ')
+ano = input('ano que voce nasceu: ')
+hora = input('hora que voce nasceu: ')
+minutos = input('minutos: ')
+segundos =input('segundos: ')
+
+print('data exata que voce nasceu; %s/%s/%s \n/%sh:%sm:%ss ' % (dia, mes , ano, hora, minutos,segundos))
