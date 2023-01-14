199d945ff929342f17f04a43ceb6d2b8936c82a4
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-14 18:21:33 -0300

    Update ex025.py

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,10 +1,13 @@
-soma_atual = 0
-numeros_lidos = 0
-while (numeros_lidos < 5):
-    num_str = input('digite um valor:')
-    num_lido = float(num_str)
-    soma_atual += num_lido
-    numeros_lidos += 1
+"""
+crie um programa que receba 5 numeros e retorne a média aritimética entres eles.
+"""
+soma_atual = 0 #criar variável
+numeros_lidos = 0 # var pra controlar num lidos
+while (numeros_lidos < 5): # ate quantos numeros
+    num_str = input('digite um valor:')# entrada numeros
+    num_lido = float(num_str) # mudar para float
+    soma_atual += num_lido # somar 0 com numero digitado
+    numeros_lidos += 1 # controle de fim do laço
 
 media = soma_atual / 5
-print('a media é %.2f ' % media)
+print('A media é %.2f ' % (media))
