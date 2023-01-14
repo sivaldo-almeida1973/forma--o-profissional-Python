5ecbc04bb519c74e9ea0bfc7f22a6a151f12c848
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-14 12:46:52 -0300

    Create aula018break.py

---
+++ aula018break.py
@@ -0,0 +1,21 @@
+# break / continua
+#num_atual = 0
+#while (True):
+ #   if(num_atual == 5):
+ #       break
+ #   print(num_atual)
+ #   num_atual += 1
+
+#print('encerrou!!')
+
+print('-'*20)
+
+num_atual = 0
+while (num_atual < 10):
+    num_atual += 1
+    if (num_atual == 7):
+        continue
+    print(num_atual)
+
+print('encerrou')
+
