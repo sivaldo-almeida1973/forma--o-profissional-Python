9551f5d8fd6918eb2809396919a6450cf4e78d33
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-15 10:01:20 -0300

    Create aula020.py

---
+++ aula020.py
@@ -0,0 +1,15 @@
+#FOR
+#texto = 'Olá ,mundo.'
+#for x in  texto:
+ #   print(x)
+#print('-'*20)
+
+#for x in range(0,3):
+  #  for b in range(0,5):
+ #     print(x,b)
+#print('-'*20)
+#TABUADA
+for x in range(1,11):
+    print('-'*20)
+    for y in range(1,11):
+        print('%d X %d = %d' % (x,y,x*y))
