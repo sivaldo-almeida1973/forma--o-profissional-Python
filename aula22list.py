15b7d8220428ac1ba553597694afac699bd587f3
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-15 15:52:21 -0300

    Create aula22list.py

---
+++ aula22list.py
@@ -0,0 +1,36 @@
+#listas(list)
+#array = []
+#array = list()
+
+#array_numeros = [1, 2, 3]
+#array_floats = [56.3, -2.2, 0.5]
+#array_str = ['a', 'b','c']
+#array_misto = [2, 2.3, 'ABC']
+
+#print(array)
+#print(array_numeros)
+#print(array_floats)
+#print(array_str)
+#print(array_misto)
+
+#color = [1,2,3,4,5]
+#print(color)
+
+array = list([1,2,3])#lista ordenada
+print(array)#imprimi array
+primeiro_elemento = array[0]#imprimir posit (0)
+print(primeiro_elemento)
+
+array[0] = 20#alterar elem pos (0) por (20)
+primeiro_elemento = array[0]#ler elemnt pos (0)
+print(primeiro_elemento)#print primeiro_el
+print(array[-1])#print array position (-1)
+print(array)#print de todo array alterado
+
+print('-'*30)
+
+array = [1, 'texto', 3]
+print(array[1:3])#imprimir da position 1 á 3
+
+
+
