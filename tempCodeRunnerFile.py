7199e3d18bb55e5bffd880622d0869368ca94303
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-15 09:01:11 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-
+letra = input('digite uma letra: ' )
+texto = input('digite uma texto: ' )
+for i in range(0, len(texto)):
+    if letra == texto[i]:
+        
+        print('encontrei a letra %s na posição %d '% (letra, i))
