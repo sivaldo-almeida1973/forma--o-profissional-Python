bc43dd36b7dd88dc3302e2daf88782efef44f2b9
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-16 09:20:00 -0300

    Create aula025.py

---
+++ aula025.py
@@ -0,0 +1,15 @@
+#lista dentro de lista
+lista = [[4,3,2,1], [1,2,3,4,5]]
+primeira_lista = lista[0]
+segunda_lista = lista[1]
+print(primeira_lista)
+print(segunda_lista)
+
+print('-'*20)
+primeiro_elem_primeira_lista = lista[0][0]
+print(primeiro_elem_primeira_lista)
+
+print('-'*20)
+
+primeiro_elem_segunda_lista = lista[1][2]
+print(primeiro_elem_segunda_lista)
