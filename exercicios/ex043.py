de92086f685505479bc1a709b0010860789df0b2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-17 18:29:13 -0300

    Update ex043.py

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -3,5 +3,5 @@ crie um lista contendo todos os numeros de 0 a
 100, mas filtre,mantendo apenas os que termi-
 nam em zero.
 """
-lista = [x for x in range(0,100) if str(x)[-1] == '0' ]
+lista = [x for x in range(0,101) if str(x)[-1] == '0' ]
 print(lista)
