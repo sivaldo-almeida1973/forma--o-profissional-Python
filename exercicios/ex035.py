e8652b736aaad4dc17ae34bc957b7d5579507656
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-17 12:30:29 -0300

    Create ex035.py

---
+++ exercicios/ex035.py
@@ -0,0 +1,12 @@
+"""
+crie uma lista com os seguintes numeros(1,10,6,7,8,
+10),em seguida printe a soma destes.
+"""
+
+tupla = (1,10,6,7,8,10)
+soma = 0
+for i in tupla:
+    soma += i
+print('Soma:',soma)
+
+
