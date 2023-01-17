ea4c03af09d9af0f473e37ad8bbdbffc4cd9c48d
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-16 21:53:32 -0300

    Create aula030dict.py

---
+++ aula030dict.py
@@ -0,0 +1,22 @@
+dados_por_nome = {
+     'ana':{
+         'sexo':'feminino',
+         'cpf':'12312312312',
+         'Rg':'12312312'
+    },
+
+     'maria':{
+         'sexo':'feminino',
+         'cpf':'12312312555',
+         'Rg':'123125799'
+    },
+
+     'lucas':{
+         'sexo':'masculino',
+         'cpf':'1231231333',
+         'Rg':'12312312'
+    }
+}
+print('Ana:',dados_por_nome['ana']['Rg'])
+print('maria:',dados_por_nome['maria']['cpf'])
+print('luca:',dados_por_nome['lucas']['sexo'])
