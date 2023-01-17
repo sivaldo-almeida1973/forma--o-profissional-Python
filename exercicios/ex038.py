663b71c1f11c4c8a86e0866fcfd5ea4a9fa83bca
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-17 17:33:21 -0300

    Create ex038.py

---
+++ exercicios/ex038.py
@@ -0,0 +1,3 @@
+lista_mes = ('janeiro','fevereiro',' março', 'abril' ,'maio' ,'junho','julho','agosto','setembro','outubro','novembro','dezembro')
+mes_nasci = int(input('digite o mes de nascimento: '))
+print('Voce nasceu em %s ' % (lista_mes[mes_nasci - 1]))
