f0f13e1e6cfeac5c065af7c095e2b0fb10a42106
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-18 18:08:00 -0300

    Create ex045.py

---
+++ exercicios/ex045.py
@@ -0,0 +1,14 @@
+"""
+criar fução chamada "e_negativo" que receba 
+um numero, retorne um booleano "True" se 
+negativo caso contrario false
+
+"""
+
+def e_negativo(num):
+    return num < 0
+
+print(e_negativo(2))
+print(e_negativo(-2))
+
+    
