acd72cfbea0ecad8b32b836d123d403f805fd2c4
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-19 11:59:14 -0300

    Create aula040calcul.py

---
+++ aula040calcul.py
@@ -0,0 +1,45 @@
+num1 = -10
+num2 = 10
+print(abs(num1))
+print(abs(num2))
+print(abs(num1) + abs(num2))
+
+print('-'*20)
+#iprime o maior valor
+maior_valor = max(10,20,30)
+print(maior_valor)
+
+lista = [-20,1,2.3,3]
+print(max(lista))
+
+#imprime menor valor
+lista = [-20,1,2.3,3]
+print(min(lista))
+
+#potencia de um numero
+x = 2
+y = 3
+print(2**3)
+
+#ou assim tambem (potencia)
+x= 2
+y = 3
+print(pow(x,y))# 2 elevado a 3
+
+#raiz quadrada (import o math)
+
+import math
+print(math.sqrt(25))
+
+#arredondamento de numeros (round)
+print(round(2.33445567, 3))
+
+#arredondamento 
+from math import floor , ceil
+print(floor(2.33445567)) # para baixo
+print(ceil(2.33445567)) #para cima
+
+#saber o quociente e o resto da divisão de 10/4
+print(divmod(10,4))
+
+
