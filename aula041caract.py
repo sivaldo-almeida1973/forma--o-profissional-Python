eba2787e560fbf5003f84050c28884cd850e5ce7
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-19 16:03:19 -0300

    Create aula041caract.py

---
+++ aula041caract.py
@@ -0,0 +1,17 @@
+#Caracteres / Numeros e Numeros / Caracteres
+numero = 70
+caractere = chr(numero)
+print('o numero %d é mapeado para o caractere %s' %(numero, caractere))
+
+
+#como gerar caracteres
+print('-'*20)
+for i in range(5,50):
+    caractere = chr(i)
+    print('%d - %s' %(i, caractere), end='\n')
+
+#codigo de caracteres
+
+caractere = 'A'
+numero = ord(caractere)
+print('O caractere %s se mapeia para o numero %d'%(caractere, numero))
