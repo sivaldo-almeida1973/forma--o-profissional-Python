b624f1b47518466a4dc2be72770b26fadd1ca668
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-20 16:54:50 -0300

    Create ex055.py

---
+++ exercicios/ex055.py
@@ -0,0 +1,13 @@
+"""
+crie uma funçao que retorne a subtração de 
+dois elementos, mas que considere o valor 
+absoluto destes valores.
+
+"""
+
+def subtrai (num1,num2):#criar função
+    return (abs(num1) - abs(num2))#retorno
+
+print(subtrai(500,-100))
+
+
