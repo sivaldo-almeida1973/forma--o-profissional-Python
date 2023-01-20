bb4c027ac14b1058bb863edd0da56631c6e1b2a7
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-20 18:48:49 -0300

    Create ex059.py

---
+++ exercicios/ex059.py
@@ -0,0 +1,7 @@
+#crie uma função que receba uma string, e para
+# cada letra minuscula a transforme em um #maiuscula e vice versa.
+
+def troca (texto):
+    return texto.swapcase()
+
+print(troca('Ola Mundo'))
