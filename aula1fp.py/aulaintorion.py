b22c55ac98b95ef3a81d5f82bbba6bfb69eed274
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-21 09:21:00 -0300

    Create aulaintorion.py

---
+++ aula1fp.py/aulaintorion.py
@@ -0,0 +1,14 @@
+#programação procedural voce terá esse tipo
+def cadastra_func(nome,dt_nascimento, salário, cargo):
+    def contrata(codigofunc):
+        return
+
+
+    def calculafolha(func,data):
+        return
+
+
+
+    
+    def demissão(func,dta):
+        return
