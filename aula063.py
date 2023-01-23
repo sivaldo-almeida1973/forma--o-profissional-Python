497ea4ff941f3ad3803765fb5d44473f72c7c8c1
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-23 20:15:31 -0300

    Create aula063.py

---
+++ aula063.py
@@ -0,0 +1,19 @@
+import teste
+
+dir(teste)
+
+
+import teste
+print(teste.PI)
+
+teste.MyFunc(10)
+
+
+from teste import PI
+print(PI)
+
+from teste import MyFunc
+MyFunc('teste')
+
+from teste import PI as Numero_PI
+print(Numero_PI)
