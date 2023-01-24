603d99cee407373c55c6a019748f253a4aa3f9e2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-24 10:05:52 -0300

    Create ex078.py

---
+++ exercicios/ex078.py
@@ -0,0 +1,7 @@
+#crie um modulo para retornar uma lista de numeros
+#aleatorios. esse modulo deve ter a seguinte #funcionalidade:
+# Uma função retorna uma lista de num randônicos #chamada de get_random_lista(inicial, final, tam), #onde 'inicial' é o num minimo que pode aparecer na #lista e 'final 'é o numero maximo que pode aparecer.
+#Por fim 'tam' deve ser o numero de elementos na #lista.Chame o modulo de 'meu_random'
+
+from get_random_lista import get_random_lista
+print(get_random_lista(1,100,10))
