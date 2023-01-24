f0fa668d66500545e6d2843874304ab2a4cbf26b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-24 09:46:45 -0300

    Create calc_python.py

---
+++ exercicios/calc_python.py
@@ -0,0 +1,10 @@
+'''Este modulo dispõe de funções para soma e subtraçõa de dois numeros'''
+
+def soma (num1,num2):
+  '''Retorna a soma de dois numeros'''
+  return num1 + num2
+
+def subtrai(num1, num2):
+  '''Retorna a subtração de dois numeros'''
+  return num1 - num2
+
