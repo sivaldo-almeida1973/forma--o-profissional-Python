8dee1968bfa88f205194ff6db92cf39c7d1b4273
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-23 21:33:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,13 @@
-#criando um modulo
+'''criando um modulo teste desenvolvido em aula'''
 PI = 3.1415
 
 class Teste:
+    '''esta é a classe teste'''
     def __init__(self):
         print('classe teste')
 
 
 def MyFunc(num):
+    '''esta é a função MyFunc'''
     print(num)
     
