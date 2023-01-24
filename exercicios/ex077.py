b26e98433fa22b6f0c1fea9debeed911db55f782
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-24 09:46:48 -0300

    Create ex077.py

---
+++ exercicios/ex077.py
@@ -0,0 +1,10 @@
+#crie um modulo que dispõem de duas funções , uma que #subtrai dois numeros e outa que soma dois numeros.#Importe essas fuções e as use.Não se esqueça de gerar a #documentação destas funções e do modulo e mostrar na #saida de seu programa.Chame o modulo de 'calc_python'.
+
+import calc_python 
+#print(calc_python.__doc__)
+#help(calc_python)
+#print(calc_python.soma.__doc__)
+resultado = calc_python.soma(10,20)
+print(resultado)
+resultado = calc_python.subtrai(10,20)
+print(resultado)
