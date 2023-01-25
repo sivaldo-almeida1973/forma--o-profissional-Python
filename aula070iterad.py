ed7950c4ad5fdab3977e1f01582e781d6c90740f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-25 10:19:19 -0300

    Create aula070iterad.py

---
+++ aula070iterad.py
@@ -0,0 +1,16 @@
+#Iteradores.
+"""lista = [1,2,3,4]
+iterador = iter(lista)
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+
+print(type(iterador))"""
+
+
+
+
+lista = [1,2,3,4]
+for item in lista:
+  print(item)
