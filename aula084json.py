cc7fdadb2609000dd9ef6e4f62ca58c8836c2d65
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-27 19:42:48 -0300

    Create aula084json.py

---
+++ aula084json.py
@@ -0,0 +1,42 @@
+#criar uma classe carros em formato (Json)
+
+import json
+
+class Carro:
+  def __init__(self, nome, potencia):
+    self.nome = nome
+    self.potencia = potencia
+#criar metodo estatico para salvar
+  @staticmethod
+  def salva_carros(*carros):
+    dicionario = dict()
+    for carro in carros:
+      dicionario[carro.nome] = carro.potencia
+    texto = json.dumps(dicionario, indent=4)
+    with open('carros.json', 'w') as arquivo:
+      arquivo.write(texto)
+#metodo pra ler carros
+  @staticmethod
+  def le_carros():
+    lista = []
+    texto = None
+    with open('carros.json', 'r') as arquivo:
+      texto = arquivo.read()
+    dicionario = json.loads(texto)
+    for chave in dicionario:
+      carro = Carro(chave, dicionario[chave])
+      lista.append(carro)
+    return lista
+
+carro1 = Carro('fusca', 40)
+carro2 = Carro('nivus', 450)
+carro3 = Carro('focus', 290)
+
+Carro.salva_carros(carro1, carro2, carro3)
+lista_carros = Carro.le_carros()
+
+for carro in lista_carros:
+  print(carro.nome, carro.potencia)
+
+
+
