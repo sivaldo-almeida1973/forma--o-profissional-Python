2ff005dce267c23d42c239882c3802fe7ef2c59e
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-27 10:44:34 -0300

    Update aula081csv.py

--- aula081csv.py
+++ aula081csv.py
@@ -1,27 +1,26 @@
-#arquivos Csv(formato universal)biblioteca CSV
+#arquivos Csv(formato universal)biblioteca CSV estutrurado 2 colunas
 import csv 
-with open('pessoas.csv','w') as arquivo: #criar arquivo
-  escritorCsv = csv.writer(arquivo, delimiter=',')#bojeto vai permitir criar arq
+with open('pessoas.csv','w') as arquivo: #with open (abrir arquivo)
+  escritorCsv = csv.writer(arquivo, delimiter=',')#objeto vai permitir criar arq
   escritorCsv.writerow(['id ','nome ','profissão'])# com escrever as linhas csv
-  escritorCsv.writerow(['1 ','Lucas ','advogado'])
+  escritorCsv.writerow(['1 ','Lucas ','advogado']) # metodo writerow(escrever)
   escritorCsv.writerow(['2 ','Alice ','Eng. software'])
   escritorCsv.writerow(['3 ','Sivaldo ','Eng. software'])
   escritorCsv.writerow(['4 ','Gute ','empresario'])
 
 
-#como criar arquivos csv
+#como criar arquivos csv apartir de uma lista
 import csv 
-linhas = [  ['id ','nome ','profissão'] , ['1 ','Lucas ','advogado'] ,
-            ['2 ','Alice ','Eng. software'],['3 ','Sivaldo ','Eng. software']
+linhas = [  ['id ','nome ','profissão'],['1 ','Lucas ','advogado'] ,
+            ['2 ','Alice ','Eng.software'],['3 ','Sivaldo ','Eng.software']
           ]
-
 with open('pessoas.2.csv','w')as file:
-  escritorCsv = csv.writer(file)
-  escritorCsv.writerows(linhas)
+  escritorCsv = csv.writer(file)# writer (leitora)
+  escritorCsv.writerows(linhas)#ler varias writerows(linhas gravação)
 
 #como ler arquivos csv
 import csv
-with open('pessoas.csv','r')as arquivo:
-  leitor = csv.reader(arquivo, delimiter=',')
+with open('pessoas.csv','r')as arquivo: #open(abrir)
+  leitor = csv.reader(arquivo, delimiter=',')#ler arquivo(header)
   for linha in leitor:
    print(linha)
