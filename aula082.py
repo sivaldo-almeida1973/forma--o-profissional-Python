31266ee87c60dbca68f914f5f35d893c66ce649c
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-27 17:02:38 -0300

    Update aula082.py

--- aula082.py
+++ aula082.py
@@ -1,37 +1,36 @@
-
 import csv
 class Pessoa:
-  def __init__(self,id, nome, profissao):#const da classe
-    self.id = id #variaveis privadas
+  def __init__(self,id, nome, profissao):
+    self.id = id 
     self.nome = nome
     self.profissao = profissao
   
-  @staticmethod #criar metodo estatico
-  def le_pessoas(): #classe pessoa
-    pessoas = [] #inicializa lista vazia
-    with open('pessoas.csv', 'r') as arquivo:#abre arquivo 
-      leitor = csv.reader(arquivo, delimiter=',')#leitor
-      for linha in leitor: #percorre as linhas do leitor
-        pessoa = Pessoa(linha[0],linha[1],linha[2])
+  @staticmethod
+  def le_pessoas():
+    pessoas = [] #lista
+    with open('pessoas.csv','r') as arquivo:
+      leitor = csv.reader(arquivo, delimiter=',')
+      for linha in leitor:
+        pessoa = Pessoa(linha[0], linha[1], linha[2]) #item linha de cada pessoa
         pessoas.append(pessoa)
     return pessoas
-
+  
   @staticmethod
-  def salva_pessoas(*pessoas):#salvar varias inst de do bojeto
-    with open('pessoas.csv', 'w') as arquivo:
-      escritorCsv = csv.writer(arquivo, delimiter=',') 
+  def salva_pessoas(*pessoas):
+    with open('pessoas.csv','w') as arquivo:
+      escritorCsv = csv.writer(arquivo, delimiter=',')
       for pessoa in pessoas:
-        escritorCsv.writerow([pessoa.id,pessoa.nome,pessoa.profissao])
+        escritorCsv.writerow([pessoa.id, pessoa.nome, pessoa.profissao])
 
-pessoa1 = Pessoa(23,'Lucas','advogado')
-pessoa2 = Pessoa(38,'Sivaldo','engenheiro')
-pessoa3 = Pessoa(38,'Alice','engenheira')
+pessoa1 = Pessoa(23, 'josé', 'eng')
+pessoa2 = Pessoa(33, 'lucas', 'advogado')
+pessoa3 = Pessoa(38, 'alice', 'eng')
 
 Pessoa.salva_pessoas(pessoa1, pessoa2, pessoa3)
 
 lista_pessoa = Pessoa.le_pessoas()
 
 for pessoa in lista_pessoa:
- print(pessoa.id, pessoa.nome, pessoa.profissao)
+  print(pessoa.id, pessoa.nome, pessoa.profissao)
 
-  
+
