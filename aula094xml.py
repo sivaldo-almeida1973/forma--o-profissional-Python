1230ae35402b6520d3b94f7b2bdb03df4744b059
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-28 15:36:15 -0300

    Create aula094xml.py

---
+++ aula094xml.py
@@ -0,0 +1,62 @@
+import xml.etree.ElementTree as xml
+import os
+
+def criaTagPesoa(nome, cpf, sexo, endereco):
+  no_pessoa = xml.Element('Pessoa', attrib={'Nome': 'Nome'})
+  no_cpf = xml.SubElement(no_pessoa, 'CPF')
+  no_cpf.text = cpf
+
+
+  no_sexo = xml.SubElement(no_pessoa, 'Sexo')
+  no_cpf.text = sexo
+
+  no_endereco = xml.SubElement(no_pessoa, 'Endereco')
+  no_cpf.text = endereco
+
+  return no_pessoa
+
+
+dados = {
+  'lucas':{
+    'CPF': '12312312311',
+    'Sexo': 'Masculino',
+  'Endereco':'Rua Brasil',
+  'Idade': 22
+  },
+  'sivaldo':{
+    'CPF': '99876588900',
+    'Sexo': 'Masculino',
+  'Endereco':'Rua Brasil',
+  'Idade': 43
+  },
+  
+  'Alice':{
+    'CPF': '777655444333',
+    'Sexo': 'Feminino',
+  'Endereco':'Rua Brasil',
+  'Idade': 43,
+  'filhos': ['lucas']
+  }
+  
+}
+
+raiz = xml.Element('dadosPessoais')
+
+for Key in dados:
+  nome = Key
+  dados_pessoa = dados[nome]
+  cpf = dados_pessoa['CPF']
+  sexo = dados_pessoa['Sexo']
+  endereco = dados_pessoa['Endereco']
+  #idade = dados_pessoa['Idade']
+  pessoa = criaTagPesoa(nome, cpf, sexo, endereco)
+  if 'filhos' in dados_pessoa:
+    filhos = xml.SubElement(pessoa, 'filhos')
+    for filho in dados_pessoa['filhos']:
+      pessoa_filhos = xml.SubElement(filhos, 'Pessoa', attrib={'nome': filho})
+  raiz.append(pessoa)
+
+arvore = xml.ElementTree(raiz)
+with open('dados_pessoais_4.xml', 'wb')as files:
+  arvore.write(files)
+
