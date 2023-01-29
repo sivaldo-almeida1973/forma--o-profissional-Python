31aa87358f30536a7167dfaccb8acc8572ab86fd
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-01-29 09:55:35 -0300

    Create ex091.py

---
+++ exercicios/ex091.py
@@ -0,0 +1,20 @@
+#ler o seguinte arquivo
+
+class Produto:
+    def __init__(self, nome, valor):
+        self.nome = nome
+        self.valor = valor
+
+produtos = []
+
+with open('nomes2.txt' ,'rt') as arquivo:
+    for linha in arquivo:
+        indice_separa = linha.index('')
+
+        nome = linha[:indice_separa-1]
+        valor = linha[:indice_separa:len(linha)-1]
+        produto = Produto(nome, valor)
+        produtos.append(produto)
+    
+    for produto in produtos:
+        print(produto.nome, produto.valor)
