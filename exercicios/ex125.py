82ae10b7b2ef2c5f128193b48bc6430f299557e4
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-01 17:58:45 -0300

    Update ex125.py

--- exercicios/ex125.py
+++ exercicios/ex125.py
@@ -1,4 +1,4 @@
-#Crie um array com os numeros de 1 a 20 , escolhendo o tipo de dado que ocupara o menor espaço da memoria. Em seguida imprima o quanto ele ocupou em bytes.
+#Crie um array com os numeros de 1 a 20 , escolhendo o tipo #de dado que ocupara o menor espaço da memoria. Em seguida #imprima o quanto ele ocupou em bytes.
 import numpy
 array = numpy.arange(1,21 , dtype=numpy.int8)
 print(array)
