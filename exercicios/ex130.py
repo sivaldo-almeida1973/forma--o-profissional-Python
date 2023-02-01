7f663849ec40447d2d9cf6b101ce71955b62f4fc
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-01 18:34:40 -0300

    Create ex130.py

---
+++ exercicios/ex130.py
@@ -0,0 +1,5 @@
+#Crie um array 4x9 com os valores quaisquer,redimensione para as dimensões 36, e 6x6.
+import numpy
+array = numpy.random.randint(0,10,(4,9))
+print(array.reshape((36)))
+print(array.reshape((6,6)))
