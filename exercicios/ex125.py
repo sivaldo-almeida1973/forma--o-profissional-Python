4866b3d53eeb45fa53e753064be172d33224634b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-01 17:53:21 -0300

    Create ex125.py

---
+++ exercicios/ex125.py
@@ -0,0 +1,5 @@
+#Crie um array com os numeros de 1 a 20 , escolhendo o tipo de dado que ocupara o menor espaço da memoria. Em seguida imprima o quanto ele ocupou em bytes.
+import numpy
+array = numpy.arange(1,21 , dtype=numpy.int8)
+print(array)
+print(array.nbytes, 'bytes')
