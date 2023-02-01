de8b1767d51e9243a492b31ca3ecff55b3859bab
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-01 18:17:06 -0300

    Create ex128.py

---
+++ exercicios/ex128.py
@@ -0,0 +1,7 @@
+#Crie uma matriz 3x3 aleatoria. precorra linha por linha #com um laço e imprima a soma de cada linha.
+
+import numpy
+array = numpy.random.randint(1,50,(3,3))
+print(array)
+for linha in array:
+  print(numpy.sum(linha))
