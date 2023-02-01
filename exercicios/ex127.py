94707cc0bdffa3665d0adeac20c207bfbad74485
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-01 18:07:20 -0300

    Create ex127.py

---
+++ exercicios/ex127.py
@@ -0,0 +1,6 @@
+#gere uma array 3x3 com numeros inteiros aleatorios entre 5 e 20.Então imprima a primeira e a ultima linha.
+import numpy
+array = numpy.random.randint(5,20,(3,3))
+print(array, end='\n\n')
+print(array[:,0] , end='\n\n')#primeira
+print(array[2,:], end='\n\n')#ultima linha
