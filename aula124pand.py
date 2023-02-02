18119e90969f3d18ae847b7a36f65df3163fd413
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-02 16:34:05 -0300

    Update aula124pand.py

--- aula124pand.py
+++ aula124pand.py
@@ -10,6 +10,7 @@ print(len(data.index))
 print(data.index)
 print(data.shape)#fromato
 
+print('-'*30)
 #a cada interaçao 
 for indice, linha in data.iterrows():
   print(indice, linha[0], linha[1])
@@ -20,9 +21,17 @@ for coluna in data:
 #acessar todos os valores de uma determinada coluna
 print(data['profissao'])
 
-
+print('-'*30)
 for coluna in data:
   print(data[coluna])
+  
+print('-'*30)
+print(data.loc['Sivaldo']['profissao'])
+print(data.loc['Sivaldo'][0])
+
+print('-'*30)
+print(data.iloc[0]['profissao'])
+print(data.iloc[0][0])
 
 
 
