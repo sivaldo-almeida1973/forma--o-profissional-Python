8559fb9d256890efe237739c94647e67aaaae986
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-03 16:14:47 -0300

    Update aula125.py

--- aula125.py
+++ aula125.py
@@ -5,7 +5,7 @@ with open('funcionarios.csv', 'w') as arquivo:
   escritorCsv.writerow(['Idade','Nome','Peso', 'Altura','profissao','endereco'])
   escritorCsv.writerow(['22','Lucas','78.0', '1.76','Advogado','Rua Brasil'])
   escritorCsv.writerow(['27','Alice','63.5', '1.60','Eng.software','Rua Brasil'])
-  escritorCsv.writerow(['35','Sivaldo','85.5', '1.70','Eng. software','Rua Brasil'])
+  escritorCsv.writerow(['35','Sivaldo','85.5', '1.70','Eng.software','Rua Brasil'])
   escritorCsv.writerow(['34','Gute','89.9', '1.65','Empresario','Rua Canada'])
   escritorCsv.writerow(['50','Maria','60.0', '1.55','Comerciante','Rua Europa'])
   
