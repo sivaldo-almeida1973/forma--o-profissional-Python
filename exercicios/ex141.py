7a19a1c5b6b54b3c0c8a2cdfe1302fb1b1dc8978
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-04 08:14:04 -0300

    Create ex141.py

---
+++ exercicios/ex141.py
@@ -0,0 +1,13 @@
+#2.mostre todas as iformações da equipe Blue Race.
+
+import pandas as pd
+dados = {
+    'Sede': ['Nova Iorque', 'Sao Paulo','Nova Iorque', 'Londres','Londres'],
+    'Piloto':['Mike Ross','Sebastian Thomas','Glen Are','Michael Schum','Luiz da Silva'],
+     'Mundiais':[10,11,0,3,44],
+     'Vitorias':[321,229,12,44,1023]
+}
+data_frame = pd.DataFrame(dados, index=['X Racing','Equatorial', 'Type','Blue Race','Capa Racing'])
+
+data = data_frame.loc['Blue Race']
+print(data)
