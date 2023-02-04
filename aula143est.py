fddf1434f1fb886a69c954f3d422f386243b13f3
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-04 18:40:30 -0300

    Create aula143est.py

---
+++ aula143est.py
@@ -0,0 +1,13 @@
+#Aplicando Estilos(design) pre definidos
+['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-v0_8', 'seaborn-v0_8-bright', 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid', 'seaborn-v0_8-deep', 'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper', 'seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 'seaborn-v0_8-ticks', 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid', 'tableau-colorblind10']
+
+import matplotlib.pyplot as plt 
+import numpy
+
+x1 = numpy.arange(1,100)
+y1 = x1**2
+
+plt.style.use('classic')
+plt.grid(color= 'gray')
+plt.plot(x1,y1)
+print(plt.show())
