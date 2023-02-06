0ce97e6997319d83f960e09b5e30591f510d02a2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-06 19:46:35 -0300

    Create teste_1.py

---
+++ teste_1.py
@@ -0,0 +1,15 @@
+#aula 151 pytest
+
+import pytest
+
+@pytest.mark.primarios
+def test_par(retorna_valor):
+  assert retorna_valor  % 2 ==0
+
+@pytest.mark.primarios
+def test_par2(retorna_valor):
+  assert retorna_valor % 3 == 0
+
+@pytest.mark.parametrize('num1,num2',[(10,2),(3,2),(3,2),(12,4)])
+def test_par3(num1, num2):
+  assert num1 % num2  % 2 == 0
