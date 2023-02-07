021ba79abdba36938326dfb5cbdd2dbf8ff694ff
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-06 21:09:33 -0300

    Create aula153.py

---
+++ aula153.py
@@ -0,0 +1,18 @@
+#API: Aplication Programming interface
+#Nome generico para aplicações que trocam informações
+#Normalmente na forma de um serviço oferecido a clientes.
+#emissão de notas fiscais, pix ,serviços financ.
+#tipos de requisição (Get,Post,Put, Delete)
+#o Tipo mais comum REST(Representational State transfer)
+#Usam mesmo padrão do HTTP:
+#Jason
+#GET: retorna dados
+#Post:atualiza dados
+#Put:cria dados
+#Delete:remove dados
+#Codigos de Status
+# 200-success,300 the api is redirecting to  #a different endpoint ,400 bad request, 401 #Not authenticated, 403 Forbidden,404 not #found, 429 too many request.
+
+#Serviço Ficticio de Cotação de Moeda 
+
+import flask
