390585c63e862fb8c2eac1175bf8c56e67b64c38
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-08 14:57:16 -0300

    Create aula160.py

---
+++ aula160.py
@@ -0,0 +1,9 @@
+import psycopg2
+con = psycopg2.connect(host='database-1.cehgg8mvfi3e.us-east-1.rds.amazonaws.com',database='inventario', user='postgres',password='1234567890')
+
+
+con.autocommit = True
+cur = con.cursor()
+cur.execute("delete from arquivos ")
+con.commit()
+con.close()
